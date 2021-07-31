400f116185d2fe57d591d55658ef654f73868799
DhruvBShetty <dhruvbshetty@gmail.com>
2021-07-31 22:04:25 +0530

    zip file handling and form

--- __init__.py
+++ __init__.py
@@ -3,19 +3,30 @@ import os
 from flask import Flask
 from flaskr import db
 from flaskr import auth
-from flaskr import blog   
+from flaskr import blog  
+from flask import flash, request, redirect, url_for,render_template
+from werkzeug.utils import secure_filename
+from zipfile import ZipFile
 
 
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
+    ALLOWED_EXTENSIONS = {'txt', 'pdf','doc','docx'}
+    UPLOAD_FOLDER="/home/dhruv/Desktop/instance/uploads"
     app = Flask(__name__, instance_relative_config=True)
+   
+    app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
         # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
+        
     )
+    
+    
+   
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -41,6 +52,36 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     
     app.add_url_rule('/', endpoint='index')
+    
+    
+    def allowed_file(filename):
+        return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    @app.route("/uploaded", methods=['GET', 'POST'])
+    def upload_file():
+        if request.method == "POST":
+            
+            file = request.files["file"]
+
+            if request.files:
+               if file.filename == '':
+                   flash('No selected file')
+                   return redirect(request.url)
+               
+               
+               
+               file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+               
+               if file.filename.rsplit('.', 1)[1].lower()=="zip":
+                   with ZipFile(os.path.join(app.config["UPLOAD_FOLDER"], file.filename),"r") as zip_ref:
+                        zip_ref.extractall(app.config["UPLOAD_FOLDER"])
+               
+                   
+
+            print(file)
+          
+        return redirect(url_for('blog.index'))
     
 
 
