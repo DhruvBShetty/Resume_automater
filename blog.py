c56c3276f784b695b06aee5ccd75f568c901ee12
DhruvBShetty <dhruvbshetty@gmail.com>
2021-09-09 12:18:54 +0530

    Added sort functionality, fixed presentation, added buttons

--- blog.py
+++ blog.py
@@ -18,23 +18,39 @@ import shutil
 
 
 bp = Blueprint('blog', __name__)
+tpath="/home/dhruv/Desktop/instance/uploads"
+
 
 def score(x):
     if x>=0.5:
         score=100
     else:
-        score=round((x/0.5)*100,2)
+        score=round(x/(1-x)*100,2)
     return score
 
 @bp.route('/')
+@login_required
 def index():
     db = get_db()
+    
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username,sales, cosec, fund, operations, corpfinance, investment'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/views.html',posts=posts)
+
+@bp.route('/Fund_accountant')
+@login_required
+def fundac():
+    db=get_db()
+    posts = db.execute(
+        'SELECT fund,title'
+        ' FROM post'
+        ' ORDER BY fund DESC'
+    ).fetchall()
+    return render_template('blog/fundac.html',posts=posts)
+
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
@@ -49,7 +65,7 @@ def create():
               return redirect(request.url)
                
                
-           tpath="/home/dhruv/Desktop/instance/uploads"
+           
 
            file.save(os.path.join(tpath,file.filename))
                
@@ -60,50 +76,77 @@ def create():
            
                   
            #title = file.filename.rsplit('.', 1)[0]
-    body = "Your notes"
+        body = " "
            
-    model2 = pickle.load(open('/home/dhruv/Downloads/model2', 'rb'))
-    model3 = pickle.load(open('/home/dhruv/Downloads/model3', 'rb'))
-    model4 = pickle.load(open('/home/dhruv/Downloads/model4', 'rb'))
-    model5 = pickle.load(open('/home/dhruv/Downloads/model5', 'rb'))
-    model6 = pickle.load(open('/home/dhruv/Downloads/model6', 'rb'))
-    model7 = pickle.load(open('/home/dhruv/Downloads/model7', 'rb'))
+        model2 = pickle.load(open('/home/dhruv/Downloads/model2', 'rb'))
+        model3 = pickle.load(open('/home/dhruv/Downloads/model3', 'rb'))
+        model4 = pickle.load(open('/home/dhruv/Downloads/model4', 'rb'))
+        model5 = pickle.load(open('/home/dhruv/Downloads/model5', 'rb'))
+        model6 = pickle.load(open('/home/dhruv/Downloads/model6', 'rb'))
+        model7 = pickle.load(open('/home/dhruv/Downloads/model7', 'rb'))
            
-    X_train_tf=pickle.load(open('/home/dhruv/Downloads/Xtraintf', 'rb'))
+        X_train_tf=pickle.load(open('/home/dhruv/Downloads/Xtraintf', 'rb'))
     
-    db = get_db()       
-    countit=set()       
-    for dp, dn, filenames in os.walk(tpath):
-        for f in filenames:
-             if f in countit:
-                 continue
-             else:
-                 countit.add(f)
-             title=f.rsplit('.',1)[0]
-             thefile=os.path.join(dp,f)
+        db = get_db()       
+        countit=set()    
+        if os.path.isdir(os.path.join(tpath,file.filename)):
+           for dp, dn, filenames in os.walk(os.path.join(tpath,file.filename)):
+              for f in filenames:
+                  if f in countit:
+                      continue
+                  else:
+                     countit.add(f)
+                     title=f.rsplit('.',1)[0]
+                     thefile=os.path.join(dp,f)
                   
-             yes=textract.process(thefile)
+                     yes=textract.process(thefile)
                     
            #print(yes)
            #tfile=nlp(yes)
            #tftxt=" ".join([i.text.lower() for i in tfile.noun_chunks])
 
-             tfex=X_train_tf.transform([yes])
+                     tfex=X_train_tf.transform([yes])
 
-             pred2=model2.predict_proba(tfex)
-             pred3=model3.predict_proba(tfex)
-             pred4=model4.predict_proba(tfex)
-             pred5=model5.predict_proba(tfex)
-             pred6=model6.predict_proba(tfex)
-             pred7=model7.predict_proba(tfex)
+                     pred2=model2.predict_proba(tfex)
+                     pred3=model3.predict_proba(tfex)
+                     pred4=model4.predict_proba(tfex)
+                     pred5=model5.predict_proba(tfex)
+                     pred6=model6.predict_proba(tfex)
+                     pred7=model7.predict_proba(tfex)
         
              #db = get_db()
-             db.execute(
+                     db.execute(
                 'INSERT INTO post (title, body, author_id, sales, cosec, fund, operations, corpfinance, investment)'
                 ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
                 (title, body, g.user['id'],score(pred7[:,1][0]),score(pred3[:,1][0]),score(pred4[:,1][0]),score(pred6[:,1][0]),score(pred2[:,1][0]),score(pred5[:,1][0]))
                 )
-    db.commit()
+        else:
+            title=file.filename.rsplit('.',1)[0]
+            thefile=os.path.join(tpath,file.filename)
+                  
+            yes=textract.process(thefile)
+                    
+           #print(yes)
+           #tfile=nlp(yes)
+           #tftxt=" ".join([i.text.lower() for i in tfile.noun_chunks])
+
+            tfex=X_train_tf.transform([yes])
+
+            pred2=model2.predict_proba(tfex)
+            pred3=model3.predict_proba(tfex)
+            pred4=model4.predict_proba(tfex)
+            pred5=model5.predict_proba(tfex)
+            pred6=model6.predict_proba(tfex)
+            pred7=model7.predict_proba(tfex)
+        
+             #db = get_db()
+            db.execute(
+        'INSERT INTO post (title, body, author_id, sales, cosec, fund, operations, corpfinance, investment)'
+        ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        (title, body, g.user['id'],score(pred7[:,1][0]),score(pred3[:,1][0]),score(pred4[:,1][0]),score(pred6[:,1][0]),score(pred2[:,1][0]),score(pred5[:,1][0]))
+        )
+        
+        db.commit()
     return redirect(url_for('blog.index'))
 
 @bp.route('/index')
@@ -151,9 +194,9 @@ def update(id):
                 (title, body, id)
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for("blog.index"))
 
-    return render_template('blog/update.html', post=post)
+    return render_template("blog/update.html", post=post)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
